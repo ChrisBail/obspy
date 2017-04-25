dd7a75fe374cd010f6fbc8e8929db19eaf145f71
ChrisBail <baillard@uw.edu>
2017-04-25 11:44:38 -0700

    fixed core.py

--- obspy/io/nlloc/core.py
+++ obspy/io/nlloc/core.py
@@ -170,12 +170,7 @@ def _read_single_hypocenter(lines, coordinate_converter, original_picks):
     lines = dict([line.split(None, 1) for line in lines[:-1]])
     line = lines["SIGNATURE"]
     line = line.rstrip().split('"')[1]
-<<<<<<< HEAD
-    signature, version, date, time = line.rsplit(" ", 3)
-    signature = signature.strip()
-    creation_time = UTCDateTime.strptime(date + time, str("%d%b%Y%Hh%Mm%S"))
 
-=======
     if "NLLoc:v6" in line:
         signature=line.rsplit("obs:",1)[0].strip()
         version=line.rsplit("NLLoc:",1)[1].split(" ")[0].strip()
@@ -183,8 +178,7 @@ def _read_single_hypocenter(lines, coordinate_converter, original_picks):
     else:
         signature, version, date, time = line.rsplit(" ", 3)
     creation_time = UTCDateTime().strptime(date + time, str("%d%b%Y%Hh%Mm%S"))
-    
->>>>>>> nlloc_read
+
     if coordinate_converter:
         # maximum likelihood origin location in km info line
         line = lines["HYPOCENTER"]
